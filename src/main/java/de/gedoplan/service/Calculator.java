c47b9fefb9229eb144e63a99dcd312a2d7ed9771
AaleXxx <alexander.kittelmann@gmx.de>
2016-08-19 12:24:55 +0200

    Operationen für Addition und Subtraktion hinzugefügt

---
+++ src/main/java/de/gedoplan/service/Calculator.java
@@ -0,0 +1,13 @@
+package de.gedoplan.service;
+
+public class Calculator
+{
+  
+  public Integer addieren(Integer value1, Integer value2) {
+    return value1 + value2;
+  }
+  
+  public Integer subtrahieren(Integer value1, Integer value2) {
+    return value1 - value2;
+  }
+}

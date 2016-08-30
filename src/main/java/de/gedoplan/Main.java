baf5055b914ed2b98c2f5ed2ec580f3f5bbea5eb
AaleXxx <alexander.kittelmann@gmx.de>
2016-08-30 21:11:42 +0200

    .

--- src/main/java/de/gedoplan/Main.java
+++ src/main/java/de/gedoplan/Main.java
@@ -12,6 +12,6 @@ public class Main
     System.out.println(calculator.addieren(2, 3));
     System.out.println(calculator.subtrahieren(2, 3));
     System.out.println(calculator.multiplizieren(2, 3));
-    System.out.println(calculator.dividieren(2, 3));
+    System.out.println(calculator.dividieren(3, 3));
   }
 }
